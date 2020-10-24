179cfe54c1aa476f043335b3ae8dc08e4f143ab8
Lama <Lama@Lama.lan>
2020-10-24 14:44:02 +0300

    test commit lama 2

--- src/jobsearchapp/JobSearchApp.java
+++ src/jobsearchapp/JobSearchApp.java
@@ -18,6 +18,7 @@ public class JobSearchApp {
         // TODO code application logic here
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
     
 }
