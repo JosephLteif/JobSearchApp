483d2427405865dd5e331faafe3544af896fef4b
LAMA\Lama <201811294@ua.edu.lb>
2020-12-02 12:51:01 +0200

    Commit by Lama: Changes on RepoUser

--- src/Repositories/RepoUser.java
+++ src/Repositories/RepoUser.java
@@ -35,7 +35,7 @@ public class RepoUser {
 
     public static void GetAll(String name) {
         try {
-            con = MySQLConnectionManager.getConnection();
+            
             ps = con.prepareStatement("SELECT * FROM user where firstName like ?;");
             ps.setString(1,  "%" + name + "%");
             rs = ps.executeQuery();
@@ -58,7 +58,7 @@ public class RepoUser {
     public static User Get(String mail) {
         User user = null;
         try {
-            con = MySQLConnectionManager.getConnection();
+            
             stmt = con.createStatement();
             rs = stmt.executeQuery("SELECT * FROM user WHERE email='" + mail + "';");
             if (rs.next()) {
@@ -68,11 +68,7 @@ public class RepoUser {
             System.out.println(ex);
 
         }
-        try {
-            con.close();
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
+        
         return user;
 
     }
@@ -89,11 +85,7 @@ public class RepoUser {
         user.setGender(rs.getInt("gender"));
         user.setLocation(rs.getString("location"));
         user.setPP(rs.getString("profilePicture"));
-        try {
-            con.close();
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
+      
         return user;
 
     }
@@ -101,7 +93,7 @@ public class RepoUser {
     public static boolean updatePass(String email, String pass) {
 
         try {
-            con = MySQLConnectionManager.getConnection();
+            
             ps = con.prepareStatement("Update user Set password=SHA(?) where email=LOWER(?);");
             ps.setString(1, pass);
             ps.setString(2, email);
@@ -119,11 +111,7 @@ public class RepoUser {
             System.out.println(throwables.getMessage());
         }
 
-        try {
-            con.close();
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
+        
         return false;
 
     }
@@ -131,7 +119,7 @@ public class RepoUser {
     private static boolean foundmail(String mail) throws SQLException {
 
         try {
-            con = MySQLConnectionManager.getConnection();
+            
             ps = con.prepareStatement("SELECT * FROM user WHERE email=Lower(?);");
             ps.setString(1, mail);
             rs = ps.executeQuery();
@@ -153,11 +141,7 @@ public class RepoUser {
                 Component frame = null;
                 JOptionPane.showMessageDialog(frame, "An account is already associated with this mail!",
                         "Account not found", JOptionPane.ERROR_MESSAGE);
-                try {
-                    con.close();
-                } catch (SQLException ex) {
-                    System.out.println(ex.getMessage());
-                }
+               
                 return false;
             }
             String SQLQuery = "INSERT INTO user (firstName,lastName,email,password,gender) values (?, ?, LOWER(?), SHA(?), ?);";
@@ -173,21 +157,13 @@ public class RepoUser {
             if (rowCreate == 1) {
 
                 RepoPasswordReset.insert(pr);
-                try {
-                    con.close();
-                } catch (SQLException ex) {
-                    System.out.println(ex.getMessage());
-                }
+                
                 return true;
             }
         } catch (SQLException ex) {
             System.out.println(ex);
         }
-        try {
-            con.close();
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
+       
         return false;
     }
 
@@ -220,21 +196,44 @@ public class RepoUser {
             done = ps.executeUpdate();
             if (done == 1) {
                 System.out.println("Done!!!!");
-                try {
-                    con.close();
-                } catch (SQLException ex) {
-                    System.out.println(ex.getMessage());
-                }
+                
                 return true;
             }
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
-        try {
-            con.close();
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
+        
         return false;
     }
+    
+    
+    
+        public void Destroy(){
+        if(rs!=null){
+            try{
+                rs.close();
+            }catch(SQLException ex){
+                System.out.println(ex);
+            }
+        }
+        if(ps!=null){
+            try{
+                ps.close();
+            }catch(SQLException ex){
+                System.out.println(ex);
+            }
+        }
+        if(stmt!=null){
+            try{
+            stmt.close();
+            
+        }catch(SQLException ex){
+                System.out.println(ex);
+                }
+        
+        
+    }
+    }
+
+   
 }
